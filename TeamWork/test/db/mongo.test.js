8faa3b81cecbfa941c80a8d3dd8d387b930329c5
Emrah Öz <emr550maranello@gmail.com>
2019-02-18 21:19:52 +0300

    final commit

--- TeamWork/test/db/mongo.test.js
+++ TeamWork/test/db/mongo.test.js
@@ -2,6 +2,262 @@ var mongo = require("../../src/db/mongo");
 var sinon = require("sinon");
 var assert = require("assert");
 
+describe("mongo module", function () {
+    var cSpyIsConnected = null;
+    var cSpyDb = null;
+    afterEach(() => {
+        if (cSpyIsConnected != null)
+            cSpyIsConnected.restore();
+        if (cSpyDb != null)
+            cSpyDb.restore();
+
+    })
+    describe(" DeleteDB", function () {
+        it(" should return resultset for given query", function (done) {
+            cSpyIsConnected = sinon.stub(mongo.client, "isConnected").callsFake(function (number) {
+                return true;
+            });
+            cSpyDb = sinon.stub(mongo.client, "db").callsFake(
+                function (dbname) {
+                    return {
+                        collection: function (table) {
+                            return {
+                                deleteMany: function (query, callback) {
+                                    callback(null,{});
+                                }
+                            }
+                        }
+                    };
+                });
+                mongo.DeleteDB("users", { username: "emr550m" }).then((result) => {
+                    done();
+                }).catch((err) => {
+                    done(err)
+                })
+
+        });
+    });
+    describe(" DeleteDB", function () {
+        it(" should return resultset for given query", function (done) {
+            cSpyIsConnected = sinon.stub(mongo.client, "isConnected").callsFake(function (number) {
+                return true;
+            });
+            cSpyDb = sinon.stub(mongo.client, "db").callsFake(
+                function (dbname) {
+                    return {
+                        collection: function (table) {
+                            return {
+                                deleteMany: function (query, callback) {
+                                    callback("Delete error occurred",{});
+                                }
+                            }
+                        }
+                    };
+                });
+                mongo.DeleteDB("users", { username: "emr550m" }).then((result) => {
+                    done("This should not succeed, it has to fail.");
+                }).catch((err) => {
+                    done();
+                })
+
+        });
+    });
+    describe(" DeleteDB", function () {
+        it(" should return resultset for given query", function (done) {
+            cSpyIsConnected = sinon.stub(mongo.client, "isConnected").callsFake(function (number) {
+                return false;
+            }); 
+                mongo.DeleteDB("users", { username: "emr550m" }).then((result) => {
+                    done("This should not succeed, it has to fail.");
+                }).catch((err) => {
+                    done();
+                })
+
+        });
+    });
+});
+
+
+
+describe("mongo module", function () {
+    var cSpyIsConnected = null;
+    var cSpyDb = null;
+    afterEach(() => {
+        if (cSpyIsConnected != null)
+            cSpyIsConnected.restore();
+        if (cSpyDb != null)
+            cSpyDb.restore();
+
+    })
+    describe(" UpdateDB", function () {
+        it(" should return resultset for given query", function (done) {
+            cSpyIsConnected = sinon.stub(mongo.client, "isConnected").callsFake(function (number) {
+                return true;
+            });
+            cSpyDb = sinon.stub(mongo.client, "db").callsFake(
+                function (dbname) {
+                    return {
+                        collection: function (table) {
+                            return {
+                                updateMany: function (query,values , callback) {
+                                    callback(null,{});
+                                }
+                            }
+                        }
+                    };
+                });
+                mongo.UpdateDB("users", { username: "emr550m" }, { name: "emrah"}).then((result) => {
+                    done();
+                }).catch((err) => {
+                    done(err)
+                })
+
+        });
+    });
+    describe(" UpdateDB", function () {
+        it(" should fail for resultset for given query", function (done) {
+            cSpyIsConnected = sinon.stub(mongo.client, "isConnected").callsFake(function (number) {
+                return true;
+            });
+            cSpyDb = sinon.stub(mongo.client, "db").callsFake(
+                function (dbname) {
+                    return {
+                        collection: function (table) {
+                            return {
+                                updateMany: function (query,values , callback) {
+                                    callback("error happened",{});
+                                }
+                            }
+                        }
+                    };
+                });
+                mongo.UpdateDB("users", { username: "emr550m" }, { name: "emrah"}).then((result) => {
+                    done("This should not succeed, it has to fail.");
+                }).catch((err) => {
+                    done()
+                })
+
+        });
+    });
+    describe(" UpdateDB", function () {
+        it(" should fail if there is no db connection", function (done) {
+            cSpyIsConnected = sinon.stub(mongo.client, "isConnected").callsFake(function (number) {
+                return false;
+            });
+            
+                mongo.UpdateDB("users", { username: "emr550m" }, { name: "emrah"}).then((result) => {
+                    done("This should not succeed, it has to fail.");
+                }).catch((err) => {
+                    done()
+                })
+
+        });
+    });
+});
+
+describe("mongo module", function () {
+    var cSpyIsConnected = null;
+    var cSpyDb = null;
+    afterEach(() => {
+        if (cSpyIsConnected != null)
+            cSpyIsConnected.restore();
+        if (cSpyDb != null)
+            cSpyDb.restore();
+
+    })
+    describe(" SelectDB", function () {
+        it(" should return resultset for given query", function (done) {
+            cSpyIsConnected = sinon.stub(mongo.client, "isConnected").callsFake(function (number) {
+                return true;
+            });
+            cSpyDb = sinon.stub(mongo.client, "db").callsFake(
+                function (dbname) {
+                    return {
+                        collection: function (table) {
+                            return {
+                                find: function (query) {
+                                    return {
+                                        project: function (fields) {
+                                            return {
+                                                toArray: function (callback) {
+                                                    callback(null, [
+                                                        {}, {}, {}
+                                                    ])
+                                                }
+                                            }
+
+                                        }
+                                    }
+
+                                }
+                            }
+                        }
+                    };
+                });
+
+
+            mongo.SelectDB("users", { username: "emr550m" }, {}).then((result) => {
+                done();
+            }).catch((err) => {
+                done(err)
+            })
+
+        });
+        it(" should return error for db reject", function (done) {
+            cSpyIsConnected = sinon.stub(mongo.client, "isConnected").callsFake(function (number) {
+                return true;
+            });
+            cSpyDb = sinon.stub(mongo.client, "db").callsFake(
+                function (dbname) {
+                    return {
+                        collection: function (table) {
+                            return {
+                                find: function (query) {
+                                    return {
+                                        project: function (fields) {
+                                            return {
+                                                toArray: function (callback) {
+                                                    callback("error occurred", [
+                                                        {}, {}, {}
+                                                    ])
+                                                }
+                                            }
+
+                                        }
+                                    }
+
+                                }
+                            }
+                        }
+                    };
+                });
+
+
+            mongo.SelectDB("users", { username: "emr550m" }, {}).then((result) => {
+                done("This should not happen, this needs to be failed.");
+            }).catch((err) => {
+                done()
+            })
+
+        });
+        it(" should return error if not connected", function (done) {
+            cSpyIsConnected = sinon.stub(mongo.client, "isConnected").callsFake(function (number) {
+                return false;
+            });
+            
+
+
+            mongo.SelectDB("users", { username: "emr550m" }, {}).then((result) => {
+                done("This should not happen, this needs to be failed.");
+            }).catch((err) => {
+                done()
+            })
+
+        });
+    });
+
+});
+
 
 describe("mongo module", function () {
     var cSpyIsConnected = null;
