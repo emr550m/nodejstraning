85718093a971e851aba84b6a3c67a4aafdc215ef
Ertan Peker <ertan.peker@gmail.com>
2019-02-19 15:27:58 +0300

    bu sefer de ok olmalı!

--- TeamWork/src/backend/auth/user/setuserinformation/index.js
+++ TeamWork/src/backend/auth/user/setuserinformation/index.js
@@ -1,13 +1,33 @@
  
+var mongo = require("../../../../../src/db/mongo");
+
 module.exports = function (requestBody) {
     return new Promise(function (resolve, reject) {
 
-     
+        //mongo.OpenDB().then(()=>{
+            mongo.SelectDB("users",{username:requestBody.username}).then((resultset)=>{
+                if(resultset.length < 1){
+                    reject({ result: false, message: 'No User Found!'  });
+                }
+                else{
 
-        resolve({
-            result:true,message:""
-        })
+                    mongo.UpdateDB("users", {username:requestBody.username}, 
+                    
+                    { $set: { name: requestBody.name, surname: requestBody.surname, 
+                        age: requestBody.age }}).then((result)=>{
+                        
+                        resolve({ result: true,  message: 'Operation Success!' });
+
+                    }).catch((err)=>{
+                        reject({ result: false, message: err });
+                    });;
+
+                } 
+            }).catch((err)=>{
+                reject({ result: false, message: err });
+            });
+        //});
+        
     });
 };
-
 
