433ff9d507ae42875c4cd79b2b0e5f05a9e734ca
Sefa Ulusoy <sefa.ulusoy@hotmail.com>
2019-02-19 15:25:16 +0300

    Sibel &Sefa

--- TeamWork/src/backend/auth/user/setuserinformation/index.js
+++ TeamWork/src/backend/auth/user/setuserinformation/index.js
@@ -1,13 +1,32 @@
- 
+
+var mongoClient = require("../../../../db/mongo");
+
 module.exports = function (requestBody) {
     return new Promise(function (resolve, reject) {
 
-     
+        var username = requestBody.username;
+        var name = requestBody.name;
+        var surname = requestBody.surname;
+        var age = requestBody.age;
 
-        resolve({
-            result:true,message:""
+        mongoClient.SelectDB("users", { username }).then((resultset) => {
+            console.log(resultset);
+            if (resultset.length > 0) {
+                return mongoClient.UpdateDB("users", { username },
+                            {  $set: { name: name, surname: surname, age: age } 
+                            }).then((result) => {
+                                resolve({ result: true,  message: 'Operation Success!' });
+                            }).catch((err)=>{
+                                reject({ result: false, message: `${err}` } )
+                            });
+            } else {
+                reject({ result: false, message: 'No User Found!'  })
+            }
+        }).catch((err) => {
+            console.log(err);
+            reject({ result: false, message: `${err}` } )
         })
-    });
+    })
 };
 
 
