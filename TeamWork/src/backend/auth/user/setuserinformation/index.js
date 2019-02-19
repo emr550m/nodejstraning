4942348443e27cc65e8315c41fa502fcc06ac316
yasinbaser <hybaser@gmail.com>
2019-02-19 15:31:52 +0300

    onsira1

--- TeamWork/src/backend/auth/user/setuserinformation/index.js
+++ TeamWork/src/backend/auth/user/setuserinformation/index.js
@@ -1,12 +1,33 @@
  
+var db = require("../../../../db/mongo");    
+
 module.exports = function (requestBody) {
+    var { username, password } = requestBody;
     return new Promise(function (resolve, reject) {
 
-     
+        db.SelectDB("users",{ username },{}).then((data) => {
+            if(data.length == 0) {
+                reject({result:false,message:"No User Found!"});
+            }
+            else {
+                //resolve({result:true,message: data});
 
-        resolve({
-            result:true,message:""
-        })
+                db.UpdateDB("users",{ username: username },{"$set":{"name":"Emrah hello","surname":"Öz","age":35}}).then((data) => {
+                    if(data.length < 0) {
+                        reject({result:false,message: "UPDATE FAILURE"});
+                    }
+                    else {
+                        resolve({result:true,message: 'Operation Success!'});
+                    }
+                    
+                }).catch((data) => {
+                    reject({result:false,message: data});
+                });
+            }
+            
+        }).catch((data) => {
+            reject({result:false,message: data});
+        });
     });
 };
 
